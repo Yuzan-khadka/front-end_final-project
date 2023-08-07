679a431893ffd56e042f1cebd6dc53ede1821581
Yuzan-khadka <yuzankhadka@gmail.com>
2023-08-07 11:45:41 -0400

    Home Page Brands and Featured Product

---
+++ js/slick-slider.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $('.brand-carousel').slick({
+      dots: false,
+      infinite: true,
+      speed: 250,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [{
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+
+        
+      ]
+    });
+  });
+  
