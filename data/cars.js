02aa1b893043f46f1700b8d93d0e57efda12cad4
umanga <Umang.sth099@gmail.com>
2023-08-09 12:51:05 -0400

    Few bug fix

---
+++ data/cars.js
@@ -0,0 +1,52 @@
+const cars = [
+    {
+      "imageSrc": "../images/banner-img-1.png",
+      "rating": 3,
+      "price": "$240.00",
+      "title": "2023 JK WRANGLER RUBICON",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui officia deserunt."
+    },
+    {
+      "imageSrc": "../images/banner-img-1.png",
+      "rating": 3,
+      "price": "$480.00",
+      "title": "2023 JK WRANGLER RUBICON",
+      "description": "Aenean gravida dignissim finibus. Nullam ipsum diam, posuere vitae pharetra sed, commodo ullamcorper."
+    },
+    {
+      "imageSrc": "../images/banner-img-1.png",
+      "rating": 5,
+      "price": "$360.00",
+      "title": "2023 JK WRANGLER RUBICON",
+      "description": "Curabitur ac mi sit amet diam luctus porta. Phasellus pulvinar sagittis diam, et scelerisque ipsum lobortis nec."
+    },
+    {
+      "imageSrc": "../images/banner-img-1.png",
+      "rating": 5,
+      "price": "$360.00",
+      "title": "2023 JK WRANGLER RUBICON",
+      "description": "Curabitur ac mi sit amet diam luctus porta. Phasellus pulvinar sagittis diam, et scelerisque ipsum lobortis nec."
+    },
+    {
+      "imageSrc": "../images/banner-img-1.png",
+      "rating": 5,
+      "price": "$360.00",
+      "title": "2023 JK WRANGLER RUBICON",
+      "description": "Curabitur ac mi sit amet diam luctus porta. Phasellus pulvinar sagittis diam, et scelerisque ipsum lobortis nec."
+    },
+    {
+      "imageSrc": "../images/banner-img-1.png",
+      "rating": 5,
+      "price": "$360.00",
+      "title": "2023 JK WRANGLER RUBICON",
+      "description": "Curabitur ac mi sit amet diam luctus porta. Phasellus pulvinar sagittis diam, et scelerisque ipsum lobortis nec."
+    },
+    {
+      "imageSrc": "../images/banner-img-1.png",
+      "rating": 5,
+      "price": "$360.00",
+      "title": "2023 JK WRANGLER RUBICON",
+      "description": "Curabitur ac mi sit amet diam luctus porta. Phasellus pulvinar sagittis diam, et scelerisque ipsum lobortis nec."
+    }
+  ]
+  
