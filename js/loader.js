bcb7581a08242c73df8421b8d8eab2ca17cce413
DESKTOP-5CJQTCE\Admin <dhruvil.se@gmail.com>
2023-08-14 17:01:28 -0400

    Loader update for full page & semi transparent bg

--- js/loader.js
+++ js/loader.js
@@ -4,12 +4,13 @@ window.addEventListener('load', function() {
     // Find the loader wrapper element
     var loaderWrapper = document.querySelector('.loader-wrapper');
   
-    // Hide the loader after at least 2 seconds
+    // Hide the loader after at least 2 seconds and remove it after the page is fully loaded
     if (loaderWrapper) {
       setTimeout(function() {
-        loaderWrapper.style.display = 'none'; // Set display to 'none' to hide the loader
-        loaderWrapper.style.zIndex = '-1';   // Set z-index to -1 to place it behind other content
-      }, 4000); // 4000 milliseconds = 4 seconds
+        loaderWrapper.style.display = 'none';
+      }, 4000); // 2000 milliseconds = 2 seconds
+  
+      document.body.style.overflow = 'auto'; // Enable scrolling once the page is fully loaded
     }
   });
   
