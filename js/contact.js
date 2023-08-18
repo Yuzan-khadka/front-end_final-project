67cb63cc0e5d4b3dc348ec5b20ff62d55e44e399
Meet Jani <janimeet2908@gmail.com>
2023-08-17 20:57:18 -0400

    responsiveness added

--- js/contact.js
+++ js/contact.js
@@ -1,35 +1,52 @@
-// contact.js
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector("form");
-    const submitButton = document.getElementById("submitButton");
-    const errorContainer = document.getElementById("errorContainer");
+// contactForm.js
+
+function handleSubmit(event) {
+    event.preventDefault();
   
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
+    const firstName = document.querySelector("#firstName").value;
+    const lastName = document.querySelector("#lastName").value;
+    const email = document.querySelector("#email").value;
+    const mobile = document.querySelector("#mobile").value;
+    const message = document.querySelector("#message").value;
   
-      // Clear previous error messages
-      errorContainer.innerHTML = "";
+    // Get error message container
+    const errorContainer = document.querySelector("#errorContainer");
+    errorContainer.innerHTML = ""; // Clear previous error messages
   
-      // Validate each input field
-      const inputs = form.querySelectorAll("input, textarea");
-      let isValid = true;
+    // Perform validation
+    if (firstName === "") {
+      showError("Please enter your first name.", errorContainer);
+      return; // Prevent further execution
+    }
+    if (lastName === "") {
+      showError("Please enter your last name.", errorContainer);
+      return;
+    }
+    if (email === "") {
+      showError("Please enter your email.", errorContainer);
+      return;
+    }
+    if (mobile === "") {
+      showError("Please enter your mobile number.", errorContainer);
+      return;
+    }
+    if (message === "") {
+      showError("Please enter your message.", errorContainer);
+      return;
+    }
   
-      inputs.forEach(input => {
-        if (!input.value.trim()) {
-          isValid = false;
-          const fieldName = input.getAttribute("placeholder");
-          const errorMessage = `${fieldName} is required.`;
-          const errorElement = document.createElement("div");
-          errorElement.className = "error-message";
-          errorElement.textContent = errorMessage;
-          errorContainer.appendChild(errorElement);
-        }
-      });
+    // If all fields are filled, proceed with form submission
+    // You can also perform AJAX request or other actions here
+    alert("Form submitted successfully!");
+  }
   
-      if (isValid) {
-        // Submit the form if validation passes
-        form.submit();
-      }
-    });
-  });
+  function showError(message, container) {
+    const errorElement = document.createElement("p");
+    errorElement.className = "error-message";
+    errorElement.textContent = message;
+    container.appendChild(errorElement);
+  }
+  
+  const submitButton = document.querySelector("#submitButton");
+  submitButton.addEventListener("click", handleSubmit);
   
